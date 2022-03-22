2bd3c23b2237ef0753039eaef6ed664f77914ccb
graphffiti <graphffiti@gmail.com>
2022-03-22 23:28:03 +0800

    Moved those echo cmd to a dedicated logging function. Add git credentials

--- install_graphffiti.sh
+++ install_graphffiti.sh
@@ -2,31 +2,43 @@
 
 ## Put shebang if needed
 
-echo "<=> Installing graphffiti workspace..."
-echo "<=>"
+log () {
+	echo "<=> $1"
+}
+
+log "Installing graphffiti workspace..."
+log ""
+
+## Setting git credentials
+git config --global user.email "graphffiti@gmail.com"
+git config --global user.name "graphffiti"
 
 if ! command -v zsh &> /dev/null 2>&1 
 then
-	echo "zsh is not yet installed!"
-	echo "Installing zsh"
+	log "zsh is not yet installed!"
+	log "Installing zsh"
 	sudo apt install zsh
 	if $? != 0; then
-		echo "Failed to install zsh :("
+		log "Failed to install zsh :("
+	else
+		log "Success installing zsh :)"
 	fi
 else
-	echo "<=> curl is already installed"
+	log "zsh is already installed"
 fi
 
 if ! command -v curl &> /dev/null 2>&1 
 then
-	echo "curl is not yet installed!"
-	echo "Installing curl"
+	log "curl is not yet installed!"
+	log "Installing curl"
 	sudo apt install curl
-	if $? != 0; then
-		echo "Failed to install curl :("
+	if [ $? != 0 ]; then
+		log "Failed to install curl :("
+	else
+		log "Success installing curl :)"
 	fi
 else
-	echo "<=> curl is already installed"
+	log "curl is already installed"
 fi
 
 
@@ -35,38 +47,46 @@ if  ! command -v rustc &> /dev/null 2>&1  ||
 	! command -v rustup &> /dev/null 2>&1 || 
 	! command -v cargo &> /dev/null 2>&1  
 then
-	echo "Rust programs are not yet installed!"
-	echo "Installing Rust"
+	log "Rust programs are not yet installed!"
+	log "Installing Rust"
 	curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-	if $? != 0; then
-		echo "Failed to install Rust :("
+	if [ $? != 0 ]; then
+		log "Failed to install Rust programs :("
+	else
+		log "Success installing Rust programs :)"
+		. "$HOME/.cargo/env"
 	fi
+
 else
-	echo "<=> Rust programs are already installed"
+	log "Rust programs are already installed"
 fi
 
 
 if ! command -v starship &> /dev/null 2>&1 
 then
-	echo "starship is not yet installed!"
-	echo "Installing starship"
+	log "starship is not yet installed!"
+	log "Installing starship"
 	cargo install starship
-	if $? != 0; then
-		echo "Failed to install starship :("
+	if [ $? != 0 ]; then
+		log "Failed to install starship :("
+	else
+		log "Success installing starship :)"
 	fi
 else
-	echo "<=> starship is already installed"
+	log "starship is already installed"
 fi
 
 if ! command -v lsd &> /dev/null 2>&1 
 then
-	echo "lsd is not yet installed!"
-	echo "Installing lsd"
+	log "lsd is not yet installed!"
+	log "Installing lsd"
 	cargo install lsd
 
-	if $? != 0; then
-		echo "Failed to install lsd :("
+	if [ $? != 0 ]; then
+		log "Failed to install lsd :("
+	else
+		log "Success installing lsd :)"
 	fi
 else
-	echo "<=> lsd is already installed"
+	log "lsd is already installed"
 fi
